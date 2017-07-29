79f0e107f5d1f955d3613a0cf3329532ca5b969f
Christopher Klimas <caklimas@gmail.com>
2017-07-29 17:51:01 -0400

    Add a form to submit the appointment date.
    In order to submit the appointment date, a Save Changes button was needed to submit the selected date along with the property id.
    
    The date picker will also not allow you to pick a date before an hour after the page was loaded. This makes setting an appointment more realistic.

--- OrangeBricks.Web/Controllers/Property/Commands/MakeAppointmentCommand.cs
+++ OrangeBricks.Web/Controllers/Property/Commands/MakeAppointmentCommand.cs
@@ -7,5 +7,14 @@ namespace OrangeBricks.Web.Controllers.Property.Commands
 {
     public class MakeAppointmentCommand
     {
+        /// <summary>
+        /// The id of the property that the appointment is for.
+        /// </summary>
+        public int PropertyId { get; set; }
+
+        /// <summary>
+        /// The date of the appointment
+        /// </summary>
+        public DateTime AppointmentDate { get; set; }
     }
 }
