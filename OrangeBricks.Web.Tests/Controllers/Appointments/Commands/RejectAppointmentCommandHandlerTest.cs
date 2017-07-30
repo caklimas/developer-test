2832101c738643f3d32227279cd525debd293c9c
Christopher Klimas <caklimas@gmail.com>
2017-07-29 23:45:28 -0400

    Add unit tests for new command handlers.
    Added some unit tests to test the Accept and Reject command handlers since they modify and update the ViewingAppointment objects.

---
+++ OrangeBricks.Web.Tests/Controllers/Appointments/Commands/RejectAppointmentCommandHandlerTest.cs
@@ -0,0 +1,52 @@
+﻿using NSubstitute;
+using NUnit.Framework;
+using OrangeBricks.Web.Controllers.Appointments.Commands;
+using OrangeBricks.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrangeBricks.Web.Tests.Controllers.Appointments.Commands
+{
+    [TestFixture]
+    public class RejectAppointmentCommandHandlerTest
+    {
+        public RejectAppointmentCommandHandler handler;
+        private IOrangeBricksContext context;
+        private IDbSet<Models.ViewingAppointment> appointments;
+
+        [SetUp]
+        public void SetUp()
+        {
+            this.context = Substitute.For<IOrangeBricksContext>();
+            this.appointments = Substitute.For<IDbSet<Models.ViewingAppointment>>();
+            this.context.ViewingAppointments.Returns(this.appointments);
+            this.handler = new RejectAppointmentCommandHandler(context);
+        }
+
+        [Test]
+        public void HandleShouldAcceptAppointment()
+        {
+            var command = new RejectAppointmentCommand
+            {
+                AppointmentId = 1
+            };
+
+            var appointment = new Models.ViewingAppointment
+            {
+                Status = ViewingAppointmentStatus.Pending
+            };
+
+            this.appointments.Find(1).Returns(appointment);
+
+            this.handler.Handle(command);
+
+            this.context.ViewingAppointments.Received(1).Find(1);
+            this.context.Received(1).SaveChanges();
+            Assert.True(appointment.Status == ViewingAppointmentStatus.Rejected);
+        }
+    }
+}
